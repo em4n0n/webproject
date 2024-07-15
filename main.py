bfaa7807a1892924216548070759f5fbf076c41c
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-15 16:05:09 -0700

    Insert html tag h1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = web.application(urls,globals())
 
 class index:
     def GET(self, name):
-        print("Hello", name, '. How are you today?')
+        return "<h1>Hello", name, '.<h1> How are you today?'
 
 if __name__ == "__main__":
     app.run()
