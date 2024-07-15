ae53c10f7222c7f6abfd60c2d4e837d01f7f8bc1
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-14 22:21:17 -0700

    Update index class to print name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import web
 
 urls = (
-    '/', 'index' # root connected to an index class
+    '/(.*)', 'index' # root connected to an index class
 )
+
+class index:
+    def GET(self, name):
+        print("Hello", name, '. How are you today?')
